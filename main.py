ceb1c80d05a3bb73a2b6db0c4807dd005dc2645f
Murtuza <murtuza.moin@gmail.com>
2026-07-19 10:25:56 +0500

    Added CORS middleware

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://news-rates-dashboard.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
